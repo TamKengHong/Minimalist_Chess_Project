5f2317296186a57451895f5394bd1b1984a00c46
Tam Keng Hong <tamkenghong@gmail.com>
2022-01-03 17:02:37 +0800

    update

--- main.py
+++ main.py
@@ -7,20 +7,23 @@ def main():
     sq_selected = ()  # no square selected, keep track of last click of user (tuple: (row, col))
     player_clicks = []  # keep track of player clicks (two tuples: [(0, 0), (0, 1)]
 
-    run = True
-    while run:
+    def refresh_screen():
         screen = gui_board()
+        draw_pieces(screen, cbd.board_state)
+        p.display.update()
+
+    screen = gui_board()
+    refresh_screen() #initialise the screen
+
+    while True:
         clock = p.time.Clock()
 
         for e in p.event.get():
             if e.type == p.QUIT:
-                run = False
                 p.quit()
                 quit()
-            elif e.type == p.MOUSEBUTTONDOWN:  # player input
-                draw_pieces(screen, cbd.board_state)
-                p.display.update()
 
+            elif e.type == p.MOUSEBUTTONDOWN:  # player input
                 if len(player_clicks) == 0:
                     sq_selected = get_sq_selected()  # no issue
                     if is_legal_piece(sq_selected, cbd):
@@ -31,19 +34,20 @@ def main():
                     if sq_selected == get_sq_selected():  # deselects if he clicks on same square
                         sq_selected = ()
                         player_clicks = []
+                        refresh_screen()
                     else:
                         sq_selected = get_sq_selected()
                         player_clicks.append(sq_selected)
 
                         if cbd.can_move(player_clicks[0], player_clicks[1]):
                             cbd.move_piece(player_clicks[0], player_clicks[1])
+                            cbd.pawn_promotion()
+                            piece = cbd.board_state[player_clicks[1][0]][player_clicks[1][1]]
+                            if isinstance(piece, Pawn) or isinstance(piece, King) or isinstance(piece, Rook):
+                                piece.first_move = False
                             cbd.whose_turn = "White" if cbd.whose_turn == "Black" else "Black"
 
-                        cbd.pawn_promotion()  # ok
-
-                        screen = gui_board()
-                        draw_pieces(screen, cbd.board_state)
-                        p.display.update()
+                        refresh_screen()
 
                         sq_selected = ()
                         player_clicks = []
